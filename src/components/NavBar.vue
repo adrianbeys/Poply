3a07dbf45ad00b4c78dfe2c9d7d82c9f2f14e47b
adrianbeys <adrian2000hx@gmail.com>
2023-03-07 09:33:19 +0100

    Generated project structure

---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  
+    <v-app-bar color="#2E4057" app>
+      <div class="logo-navbar"><img :src="logonav"></div>
+      <v-toolbar-title class="nav-title">Poply</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-tabs v-model="tab" bg-color="#FFFFFF0" color="white">
+      <v-tab value="one"><v-icon>mdi-home</v-icon>&nbsp;Home</v-tab>
+      <v-tab value="two"><v-icon>mdi-book</v-icon>&nbsp;About</v-tab>
+      <v-tab value="three"><v-icon>mdi-account-circle</v-icon>&nbsp;Create Account</v-tab>
+    </v-tabs>
+    </v-app-bar>
+    
+    <!-- <v-content>
+      <router-view></router-view>
+    </v-content> -->
+    
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  data(){
+    return {
+      logonav: require("../assets/logo-poply.svg"),
+      sidebar: false,
+      menuItems: [
+          { title: 'Home', path: '/home', icon: 'home' },
+          { title: 'Sign Up', path: '/signup', icon: 'face' },
+          { title: 'Sign In', path: '/signin', icon: 'lock_open' }
+     ]
+    }
+  },
+};
+</script>
+<style>
+  .nav-title {
+    color: white;
+    cursor: pointer;
+    font-weight: 700;
+  }
+  .logo-navbar img {
+    height: 4rem;
+    margin-left: 1rem;
+    margin-top: 0.2rem;
+  }
+  .v-toolbar__content > .v-toolbar-title {
+    margin-left: 0;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <NavBar />
+  <Homepage />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import NavBar from '../components/NavBar.vue';
+import Homepage from '../components/Homepage.vue';
+
+export default defineComponent({
+
+  beforeCreate: function() {
+        document.body.className = 'home';
+  },
+
+  name: 'HomeView',
+
+  components: {
+    NavBar,
+    Homepage,
+  },
+});
+</script>
+
+<style lang="scss">
+ .v-main {
+  background-color: #2F2D2E;
+  color: white;
+ }
+</style>
