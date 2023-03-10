d43aaa2de7dd69861717a9653c402e2ae26c7e02
OlexTix <olexix@gmail.com>
2023-03-10 14:29:16 +0100

    Created ChatBox component for ChatView WIP. Adjusted ChatView

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,163 @@
+<template >
+    <div id="chatbox" class="pa-8 mx-auto">
+        <v-container class="fill-height pa-0">
+            <v-row class="no-gutters elevation-4">
+                <v-col cols="12" sm="3">
+                    <h2 style="margin-bottom:10px;">Chat</h2>
+                    <div style="background-color:#1a1a1a; border-radius: 10px;">
+                    <v-list class="overflow-y-auto" style="background-color: transparent; " v-model="activeChat"
+                        v-for="(item, index) in parents">
+
+                        <v-virtual-scroll :items="items" :item-height="50" height="300">
+                            <v-list-item style="background-color: transparent; color: white; border-radius: 20px;" :key="`parent${index}`"
+                                :value="item.id">
+                              
+
+                                <v-list-item-title v-text="item.title" />
+                                <v-list-item-subtitle v-text="'Test message'" />
+                            </v-list-item>
+                        </v-virtual-scroll>
+                        <v-divider :key="`chatDivider${index}`" class="my-0" color="transparent" />
+                    </v-list>
+                </div>
+                </v-col>
+                <v-col cols="auto" class="flex-grow-1">
+                    <v-card flat class="d-flex flex-column fill-height" style="background-color: grey;">
+                        <v-card-title style="color:white;">
+                            Example chat
+                        </v-card-title>
+                        <v-card-text class="flex-grow-1 overflow-y-auto ma-2">
+                            <template v-for="(msg, i) in messages">
+                                <div :class="{ 'd-flex flex-row-reverse': msg.me }">
+                                    <v-menu offset-y>
+                                        <template v-slot:activator="{ on }">
+                                            <v-hover>
+                                                <v-chip :color="msg.me ? 'blue' : 'white'" dark
+                                                    style="height:auto;white-space: normal; background:#262626"
+                                                    class="pa-4 mb-2" v-on="on">
+                                                    <div id="content">{{ msg.content }}</div>
+                                                    <sub class="ml-2" style="font-size: 0.5rem; color: white;">{{
+                                                        msg.created_at }}</sub>
+                                                </v-chip>
+                                            </v-hover>
+                                        </template>
+                                    </v-menu>
+                                </div>
+                            </template>
+                        </v-card-text>
+                        <div class="d-flex pa-6">
+                            <v-textarea density="comfortable" placeholder="Write a message" type="text" rows="3"
+                            style="margin-right: 20px"
+                               variant="solo" />
+                               <v-btn style="margin-top: 15px; background-color: green; color: white;" icon="mdi-send"></v-btn>
+                      </div>
+                        
+                    </v-card>
+
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        activeChat: 1,
+        parents: [
+            {
+                id: 1,
+                title: "john doe",
+                active: true
+            },
+            {
+                id: 3,
+                title: "scarlett",
+                active: false
+            },
+            {
+                id: 4,
+                title: "scarlett",
+                active: false
+            },
+            {
+                id: 5,
+                title: "scarlett",
+                active: false
+            },
+            {
+                id: 6,
+                title: "scarlett",
+                active: false
+            },
+            {
+                id: 7,
+                title: "scarlett",
+                active: false
+            },
+            {
+                id: 8,
+                title: "scarlett",
+                active: false
+            },
+            {
+                id: 9,
+                title: "scarlett",
+                active: false
+            },
+            {
+                id: 10,
+                title: "scarlett",
+                active: false
+            },
+
+        ],
+        messages: [
+            {
+                content: "lorem ipsum",
+                me: true,
+                created_at: "11:11am"
+            },
+            {
+                content: "dolor",
+                me: false,
+                created_at: "11:11am"
+            },
+            {
+                content: "dolor",
+                me: false,
+                created_at: "11:11am"
+            },
+            {
+                content: "dolor",
+                me: false,
+                created_at: "11:11am"
+            },
+            {
+                content: "dolor",
+                me: true,
+                created_at: "11:11am"
+            },
+            {
+                content: "dolor",
+                me: false,
+                created_at: "11:12am"
+            },
+            {
+                content: "dolor",
+                me: false,
+                created_at: "11:14am"
+            }
+        ],
+    })
+
+}
+</script>
+
+
+<style>
+
+
+#content {
+    color: white;
+}
+</style>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,45 +1,44 @@
 <template>
-  <v-navigation-drawer theme="dark" expand-on-hover rail mini-variant permanent class="d-flex fill-height">
-    <v-list>
-      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="usertest"
-        subtitle="example@email.com"></v-list-item>
-    </v-list>
+  <main>
+    <v-navigation-drawer theme="dark" expand-on-hover rail mini-variant permanent class="d-flex fill-height">
+      <v-list>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="usertest"
+          subtitle="example@email.com"></v-list-item>
+      </v-list>
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <v-list density="compact" nav>
-      <v-list-item-group>
+      <v-list density="compact" nav>
         <v-list-item prepend-icon="mdi-message-text" title="Chat" value="chat"></v-list-item>
         <v-list-item prepend-icon="mdi-cog" title="Settings" value="settings"></v-list-item>
-      </v-list-item-group>
-      <v-divider></v-divider>
-      <v-list-item-group class="posbottom">
-        <v-list-item prepend-icon="mdi-logout" title="Log out" value="logout"></v-list-item>
-      </v-list-item-group>
+        <v-divider></v-divider>
+        <v-list-item prepend-icon="mdi-logout" class="posbottom text-red" title="Log out" value="logout"></v-list-item>
 
-    </v-list>
-  </v-navigation-drawer>
 
-  <v-main>
-    <div class="d-flex justify-center ma-4 pa-4">
-      <v-sheet rounded width="500" min-height="500" class="pa-4 mx-auto">
-        <div class="d-flex justify-center ma-2">
-          <RouterLink to="/">
-            <v-img width="50" height="50" src="../assets/logo-poply.svg">
-            </v-img>
-          </RouterLink>
+      </v-list>
+    </v-navigation-drawer>
 
-        </div>
-      </v-sheet>
-    </div>
-  </v-main>
+
+    <ChatBox></ChatBox>
+  </main>
 </template>
 
+<script>
+import { defineComponent } from 'vue';
+import ChatBox from '../components/ChatBox.vue';
+
+export default defineComponent({
+  beforeCreate: function () {
+    document.body.className = 'chatbox';
+  },
+  name: 'ChatView',
+  components: {
+    ChatBox,
+  },
+});
+</script>
+
 <style>
-.posbottom {
-  position: fixed;
-  bottom: 0;
-  margin-bottom: 10px;
-  width: 94%;
-}
+
+
 </style>
